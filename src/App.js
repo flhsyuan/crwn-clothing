b3e64bd0008bb374d811ff2a1105e2fd5c1b6c85
sharp <b81481002@gmail.com>
2020-04-14 21:03:04 +1000

    Add the colletion items in the shop page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
+
 import { Switch, Route } from "react-router-dom";
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
@@ -16,7 +13,7 @@ function App() {
       <Switch>
         {/* exact is true means it is the exact path to render, component is the page to go */}
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
