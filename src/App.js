26eac96b3567cce693c079a082a8c3e10c2cb8ba
sharp <b81481002@gmail.com>
2020-04-15 21:54:06 +1000

    Google login authentication

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
 import React from "react";
 import "./App.css";
 
+// the router
 import { Switch, Route } from "react-router-dom";
-
+// the components
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-
 import Header from "./components/header/header.component";
-
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-out.component";
-function App() {
-  return (
-    <div>
-      <Header />
-      {/* switch can save resource */}
-      <Switch>
-        {/* exact is true means it is the exact path to render, component is the page to go */}
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+
+//the auth
+import { auth } from "./firebase/firebae.utils";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        {/* switch can save resource */}
+        <Switch>
+          {/* exact is true means it is the exact path to render, component is the page to go */}
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
