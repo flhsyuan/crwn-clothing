4ab1feec4e27491757d05f427fdc87befa58fcc4
sharp <b81481002@gmail.com>
2020-04-22 15:30:56 +1000

    construct the selector for the cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,6 +3,7 @@ import "./cart-icon.styles.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selector";
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
@@ -13,12 +14,10 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
 
 // connect to the state and pull the state as the props into the component
 // whenever the state is updated, the mapStateToProps will run again and pops new state in pur component
-const mapStateToProps = ({ cart: { cartItems } }) => ({
+// pass the whole state into the selector
+const mapStateToProps = (state) => ({
   // pass in the parameter which is the item account
-  itemCount: cartItems.reduce(
-    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
-    0 //the initial value
-  ),
+  itemCount: selectCartItemsCount(state),
 });
 
 //pull the needed action.
---
+++ src/redux/cart/cart.selector.js
@@ -0,0 +1,23 @@
+import { createSelector } from "reselect";
+
+// input selector and output selector
+// input selector does not use the createSelector and output selector use the input selector and createSelector to build themselves
+
+//get the new state and return just a slice of it
+const selectCart = (state) => state.cart;
+
+// two arguments:1. a colleciton of input selectors.    2. the value you want out of the selector
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0 //the initial value
+    )
+);
