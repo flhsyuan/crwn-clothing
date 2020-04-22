4aa2648afb91761ce2934970442b226fac3e65c6
sharp <b81481002@gmail.com>
2020-04-22 14:41:58 +1000

    debug the add item to the cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,14 +4,25 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count"> 0 </span>
+    <span className="item-count"> {itemCount} </span>
   </div>
 );
 
+// connect to the state and pull the state as the props into the component
+// whenever the state is updated, the mapStateToProps will run again and pops new state in pur component
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  // pass in the parameter which is the item account
+  itemCount: cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0 //the initial value
+  ),
+});
+
+//pull the needed action.
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
