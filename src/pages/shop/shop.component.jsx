536f0b167f583374ca0040923e0416286db971e5
sharp <b81481002@gmail.com>
2020-04-24 17:14:21 +1000

    re-structure the shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,11 @@
 import React from "react";
 // import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/preview-collection/preview-collection.component";
-
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import { selectCollections } from "../../redux/shop/shop.selector";
+import CollectionsOverview from "../../components/collections-overview/collection-overview.component";
 
 const ShopPage = ({ collections }) => (
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-  //     // collections: SHOP_DATA,
-  //   };
-  //
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionsOverview />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
