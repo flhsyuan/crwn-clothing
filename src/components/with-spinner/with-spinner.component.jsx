e6bb94859f02e827f4679f6a855daac8b4b68f73
sharp <b81481002@gmail.com>
2020-05-09 20:29:43 +1000

    Add some comments

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { SpinnerContainer, SpinnerOverlay } from "./with-spinner.styles";
 
+//The higher order component takes an component as the input and use the parameters inside the component.
 const WithSpinner = (WrappedComponent) => {
   const Spinner = ({ isLoading, ...otherProps }) => {
     return isLoading ? (
